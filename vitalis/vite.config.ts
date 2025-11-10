29ead91e11521638ef2173e6dfcdd506cf3e129e
JoaoGuilherme <joaoguilhermecarvalhonovaes@gmail.com>
2025-11-09 22:26:35 -0300

    feat: vite ajustado

--- vitalis/vite.config.ts
+++ vitalis/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  
+  // 👇 Adicione esta seção
+  preview: {
+    allowedHosts: [
+      'portal-de-entrada-662788552627.us-central1.run.app'
+    ]
+  }
 })
